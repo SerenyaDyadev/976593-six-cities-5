d2544b63a5efabffc75e3f1dcbf7ec5b318d6a67
Serenya Dyadev <3684182@gmail.com>
2020-10-01 23:48:33 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen";
-
+import AuthScreen from "../auth-screen/auth-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import PropertyScreen from "../property-screen/property-screen";
 
 const App = (props) => {
 
   const {offersCount} = props;
 
   return (
-    <MainScreen offersCount={offersCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen errorsCount={offersCount} />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/offer/:id?">
+          <PropertyScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
