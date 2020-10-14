e5ec71e6d317e704d1d3a77b67a83bdb232741aa
Serenya Dyadev <3684182@gmail.com>
2020-10-14 15:12:11 +0300

    need help %)

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -5,7 +5,6 @@ import ListCards from "../list-cards/list-cards";
 import Map from "../map/map";
 
 const MainScreen = (props) => {
-
   const {offers} = props;
 
   return (
@@ -15,7 +14,7 @@ const MainScreen = (props) => {
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
               </a>
             </div>
             <nav className="header__nav">
@@ -32,7 +31,6 @@ const MainScreen = (props) => {
           </div>
         </div>
       </header>
-
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -73,14 +71,16 @@ const MainScreen = (props) => {
         </div>
         <div className="cities">
           {offers.length === 0 ?
-            <div className="cities__places-container cities__places-container--empty container">
-              <section className="cities__no-places">
-                <div className="cities__status-wrapper tabs__content">
-                  <b className="cities__status">No places to stay available</b>
-                  <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
-                </div>
-              </section>
-              <div className="cities__right-section"></div>
+            <div className="cities">
+              <div className="cities__places-container cities__places-container--empty container">
+                <section className="cities__no-places">
+                  <div className="cities__status-wrapper tabs__content">
+                    <b className="cities__status">No places to stay available</b>
+                    <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                  </div>
+                </section>
+                <div className="cities__right-section"></div>
+              </div>
             </div>
             :
             <div className="cities__places-container container">
@@ -89,7 +89,8 @@ const MainScreen = (props) => {
                 <b className="places__found">{offers.length} places to stay in Amsterdam</b>
                 <form className="places__sorting" action="#" method="get">
                   <span className="places__sorting-caption">Sort by</span>
-                  <span className="places__sorting-type" tabIndex="0"> Popular
+                  <span className="places__sorting-type" tabIndex="0">
+                  Popular
                     <svg className="places__sorting-arrow" width="7" height="4">
                       <use xlinkHref="#icon-arrow-select"></use>
                     </svg>
@@ -101,20 +102,13 @@ const MainScreen = (props) => {
                     <li className="places__option" tabIndex="0">Top rated first</li>
                   </ul>
                 </form>
-
-                <ListCards
-                  offers={offers}
-                />
-
+                <ListCards offers={offers}/>
               </section>
               <div className="cities__right-section">
-                <section className="cities__map map">
-                  <Map />
-                </section>
+                <Map offers={offers}/>
               </div>
             </div>
           }
-
         </div>
       </main>
     </div>
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,40 +1,50 @@
-import React from "react";
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
 import leaflet from "leaflet";
-// import "/leaflet/dist/leaflet.css";
 
+const CITY_COORDINATES = [52.38333, 4.9];
 
-const Map = () => {
+class Map extends PureComponent {
 
-  const city = [52.38333, 4.9];
+  componentDidMount() {
 
-  const icon = leaflet.icon({
-    iconUrl: `img/pin.svg`,
-    iconSize: [30, 30]
-  });
+    const icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
 
-  const zoomMap = 12;
+    const zoom = 12;
 
-  const map = leaflet.map(`map`, {
-    center: city,
-    zoom: zoomMap,
-    zoomControl: false,
-    marker: true
-  });
+    const map = leaflet.map(`map`, {
+      center: CITY_COORDINATES,
+      zoom,
+      zoomControl: false,
+      marker: true
+    });
 
-  map.setView(city, zoomMap);
+    map.setView(CITY_COORDINATES, zoom);
 
-  leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-  }).addTo(map);
+    leaflet
+      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
+      .addTo(map);
 
-  const offerCords = [52.3709553943508, 4.89309666406198];
-  leaflet
-    .marker(offerCords, {icon})
-    .addTo(map);
+    // const offerCords = [52.3709553943508, 4.89309666406198];
+    // leaflet
+    //   .marker(offerCords, {icon})
+    //   .addTo(map);
+  }
 
-  return (
-    <div id="map"></div>
-  );
+  render() {
+    return (
+      <section id="map" className="cities__map map"></section>
+    );
+  }
+}
+
+Map.propTypes = {
+  offers: PropTypes.array.isRequired,
 };
 
 export default Map;
