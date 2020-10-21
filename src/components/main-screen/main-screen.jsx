f345c4698a1d37e661968c890cb8627ea7f611e9
Serenya Dyadev <3684182@gmail.com>
2020-10-21 15:09:06 +0300

    Разбил на хэлдер и футер, доделал redux для offerScreen и Favorites

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import NoOfferScreen from "../no-offer-screen/no-offer-screen";
+import Map from "../map/map";
+import Header from "../header/header";
 import OfferList from "../offer-list/offer-list";
 import CityList from "../city-list/city-list";
 import {connect} from 'react-redux';
-import Map from "../map/map";
 import {getCityOffers} from '../../utils';
-import NoOfferScreen from "../no-offer-screen/no-offer-screen";
 
 const MainScreen = (props) => {
 
@@ -13,28 +14,8 @@ const MainScreen = (props) => {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+
+      <Header />
 
       {offers.length === 0 ?
         <NoOfferScreen
