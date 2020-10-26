51612c2f0d3793424564106ba748bf3835286315
Serenya Dyadev <3684182@gmail.com>
2020-10-26 22:42:26 +0300

    исправление пары ошибок

--- src/components/new-review-form/new-review-form.jsx
+++ src/components/new-review-form/new-review-form.jsx
@@ -4,7 +4,7 @@ import withRewievForm from "../../hocs/with-feview-form/with-review-form";
 
 const NewReviewForm = (props) => {
 
-  const {handleRatingChange, handleTextFieldChange, onSubmit} = props;
+  const {onRatingChange, onTextFieldChange, onSubmit} = props;
 
   return (
     <form className="reviews__form form" action="#" method="post"
@@ -12,7 +12,7 @@ const NewReviewForm = (props) => {
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"
-          onChange={handleRatingChange} />
+          onChange={onRatingChange} />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
@@ -20,7 +20,7 @@ const NewReviewForm = (props) => {
         </label>
 
         <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"
-          onChange={handleRatingChange} />
+          onChange={onRatingChange} />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
@@ -28,7 +28,7 @@ const NewReviewForm = (props) => {
         </label>
 
         <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"
-          onChange={handleRatingChange} />
+          onChange={onRatingChange} />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
@@ -36,7 +36,7 @@ const NewReviewForm = (props) => {
         </label>
 
         <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"
-          onChange={handleRatingChange} />
+          onChange={onRatingChange} />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
@@ -44,7 +44,7 @@ const NewReviewForm = (props) => {
         </label>
 
         <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"
-          onChange={handleRatingChange} />
+          onChange={onRatingChange} />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
@@ -52,7 +52,7 @@ const NewReviewForm = (props) => {
         </label>
       </div>
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"
-        onChange={handleTextFieldChange}></textarea>
+        onChange={onTextFieldChange}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
@@ -65,8 +65,8 @@ const NewReviewForm = (props) => {
 
 
 NewReviewForm.propTypes = {
-  handleRatingChange: PropTypes.func.isRequired,
-  handleTextFieldChange: PropTypes.func.isRequired,
+  onRatingChange: PropTypes.func.isRequired,
+  onTextFieldChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
 
