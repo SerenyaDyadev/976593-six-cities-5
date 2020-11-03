62544c49ba3bed16c5af4a7f6733705524b79e37
Serenya Dyadev <3684182@gmail.com>
2020-11-03 22:51:56 +0300

    auth-screen

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {connect} from 'react-redux';
+import {AuthorizationStatus} from "../../const";
 
 const Header = (props) => {
 
-  const login = props ? true : false;
+  const {authorizationStatus, email} = props;
 
   return (
     <header className="header">
@@ -16,11 +19,11 @@ const Header = (props) => {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                { login ?
+                {authorizationStatus !== AuthorizationStatus.NO_AUTH ?
                   <a className="header__nav-link header__nav-link--profile" href="/favorites">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    <span className="header__user-name user__name">{email}</span>
                   </a>
                   :
                   <a className="header__nav-link header__nav-link--profile" href="/login">
@@ -38,4 +41,15 @@ const Header = (props) => {
   );
 };
 
-export default Header;
+Header.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({USER}) => ({
+  authorizationStatus: USER.authorizationStatus,
+  email: USER.email
+});
+
+export {Header};
+export default connect(mapStateToProps)(Header);
