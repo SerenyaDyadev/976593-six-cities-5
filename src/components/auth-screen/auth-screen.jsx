b96f02b3f43f865e55ca43546a9838a380090512
Serenya Dyadev <3684182@gmail.com>
2020-11-03 23:15:47 +0300

    auth done кроме favorites

--- src/components/auth-screen/auth-screen.jsx
+++ src/components/auth-screen/auth-screen.jsx
@@ -1,41 +1,113 @@
-import React from "react";
+import React, {PureComponent, createRef} from "react";
+import PropTypes from "prop-types";
 import Header from "../header/header";
+import {connect} from "react-redux";
+import {login} from "../../store/api-actions";
+import {Redirect} from 'react-router-dom';
+import {AuthorizationStatus} from '../../const';
 
-const AuthScreen = () => {
+class AuthScreen extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="page page--gray page--login">
+    this.loginRef = createRef();
+    this.passwordRef = createRef();
 
-      <Header />
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required="" />
+  handleSubmit(evt) {
+    const {onSubmit} = this.props;
+
+    evt.preventDefault();
+
+    onSubmit({
+      login: this.loginRef.current.value,
+      password: this.passwordRef.current.value,
+    });
+  }
+
+  render() {
+    const {city, authorizationStatus} = this.props;
+
+    if (authorizationStatus === AuthorizationStatus.AUTH) {
+      return <Redirect to={`/`} />;
+    }
+
+    return (
+      <div className="page page--gray page--login">
+
+        <Header />
+
+        <main className="page__main page__main--login">
+          <div className="page__login-container container">
+            <section className="login">
+              <h1 className="login__title">Sign in</h1>
+
+              <form
+                onSubmit={this.handleSubmit}
+                className="login__form form"
+                action="#"
+                method="post"
+              >
+
+                <div className="login__input-wrapper form__input-wrapper">
+                  <label className="visually-hidden">E-mail</label>
+
+                  <input
+                    ref={this.loginRef}
+                    className="login__input form__input"
+                    type="email" name="email"
+                    placeholder="Email" required=""
+                  />
+
+                </div>
+                <div className="login__input-wrapper form__input-wrapper">
+                  <label className="visually-hidden">Password</label>
+
+                  <input
+                    ref={this.passwordRef}
+                    className="login__input form__input"
+                    type="password" name="password"
+                    placeholder="Password"
+                    required=""
+                  />
+
+                </div>
+
+                <button className="login__submit form__submit button" type="submit">Sign in</button>
+              </form>
+            </section>
+            <section className="locations locations--login locations--current">
+              <div className="locations__item">
+                <a className="locations__item-link" href="/" >
+                  <span>{city}</span>
+                </a>
               </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required="" />
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="/" >
-                <span>Amsterdam</span>
-              </a>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
-  );
+            </section>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+AuthScreen.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  city: PropTypes.string.isRequired,
+  authorizationStatus: PropTypes.string.isRequired,
 };
 
+const mapStateToProps = ({USER, CITIES}) => ({
+  authorizationStatus: USER.authorizationStatus,
+  city: CITIES.city
+});
 
-export default AuthScreen;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  }
+});
+
+export {AuthScreen};
+export default connect(mapStateToProps, mapDispatchToProps)(AuthScreen);
