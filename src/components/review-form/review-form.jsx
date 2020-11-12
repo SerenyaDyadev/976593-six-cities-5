f6bcd06ac5f4e6a0a6b9ddfdd12574921615d101
Serenya Dyadev <3684182@gmail.com>
2020-11-12 17:43:42 +0300

    reviewForm e2e

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -133,6 +133,7 @@ class ReviewForm extends PureComponent {
           ref={this.reviewInputRef}
           className="reviews__textarea form__textarea" id="review"
           name="review"
+          value={review}
           placeholder="Tell how was your stay, what you like and what can be improved"
           onChange={onTextFieldChange}>
         </textarea>
